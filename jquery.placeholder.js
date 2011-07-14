f087029a3bf49a3efbec53583769f6c8f572c1a0
Jethro <jethrolarson@gmail.com>
2011-07-14 16:12:08 -0700

    updated comments

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -2,7 +2,7 @@
 	//id itterator if the inputs don't have ids
 	var phid=0;
 	$.fn.placeholder = function(){
-		return this.bind({//Live doesn't work right in IE >:(
+		return this.bind({
 			focus: function(){
 				$(this).parent().addClass('placeholder-focus');
 			},blur: function(){
@@ -11,12 +11,14 @@
 				$(this).parent().toggleClass('placeholder-changed',this.value!=='');
 			}
 		}).each(function(){
-			var $this = $(this); 
-			if(!this.id) this.id='ph_'+(phid++); //Adds an id to elements if absent
+			var $this = $(this);
+			//Adds an id to elements if absent
+			if(!this.id) this.id='ph_'+(phid++); 
 			//Create input wrapper with label for placeholder. Also sets the for attribute to the id of the input if it exists.
 			$('<span class="placeholderWrap"><label for="'+this.id+'">'+$this.attr('placeholder')+'</label></span>')
-				.insertAfter($this)//Put the whole thing after the original input
-				.append($this); //Disables default placeholder
+				.insertAfter($this)
+				.append($this); 
+			//Disables default placeholder
 			$this.attr('placeholder','').keyup();
 		});
 	};
