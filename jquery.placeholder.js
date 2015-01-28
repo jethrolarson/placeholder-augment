b3e16b9168fd3e4301a421385f55e80da425a923
Jethro Larson <jethrolarson@gmail.com>
2015-01-28 15:42:12 -0800

    Update jquery.placeholder.js
    Removed legacy support for FF < 4. Cleaned some stuff a little.

--- jquery.placeholder.js
+++ jquery.placeholder.js
@@ -1,8 +1,8 @@
 //Author: @jethrolarson
 //License: MIT, GPL, or WTFPL
-(function($){
+(function($, global){
 	//id itterator if the inputs don't have ids
-	var phid=0;
+	var phid = 0;
 	$.fn.placeholder = function(){
 		return this.bind({
 			focus: function(){
@@ -10,39 +10,28 @@
 			},blur: function(){
 				$(this).parent().removeClass('placeholder-focus');
 			},'keyup input change': function(){
-				$(this).parent().toggleClass('placeholder-changed',this.value!=='');
+				$(this).parent().toggleClass('placeholder-changed', this.value);
 			}
 		}).each(function(){
 			var $this = $(this);
-			//Adds an id to elements if absent
-			if(!this.id) this.id='ph_'+(phid++); 
-			//Create input wrapper with label for placeholder. Also sets the for attribute to the id of the input if it exists.
-			$('<span class="placeholderWrap"><label for="'+this.id+'">'+$this.attr('placeholder')+'</label></span>')
-				.insertAfter($this)
-				.append($this); 
-			//Disables default placeholder
-			$this.attr('placeholder','').keyup();
+			var id = this.id || 'ph_' + (phid++)
 			
-			//fixes lack of event for autocomplete in firefox < 4:'(
-			if($.browser.mozilla && $.browser.version.slice(0,3) == "1.9"){
-				$this.focus(function(){
-					var val = this.value,
-					    el = this,
-					    $el = $(this);
-					$el.data('ph_timer', setInterval(function(){
-						if(val != el.value){
-							$el.change();
-						}
-					},100));
-				}).blur(function(){
-					clearInterval($(this).data('ph_timer'));
-				});
-			}
+			//Create input wrapper with label for placeholder. Also sets the for attribute to the id of the input if it exists.
+			var $wrapper = $('<span class="placeholderWrap"><label for="' + id + '">' + $this.attr('placeholder') + '</label></span>');
+			
+			//Disables default placeholder
+			$this.attr('placeholder', '').keyup();
+			
+			//Adds an id to elements if absent
+			this.id = id; 
+			$wrapper.insertAfter($this).append($this);
 		});
 	};
 	
 	//Default plugin invocation 
-	$(function(){
-		$('input[placeholder]').placeholder();
-	});
-})(jQuery);
+	global.initPlaceholder = global.initPlaceholder || function(){
+		$(function(){
+			$('input[placeholder]').placeholder();
+		});
+	};
+})(jQuery, this);
